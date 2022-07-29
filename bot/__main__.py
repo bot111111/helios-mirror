da7be75b9a4e69f33ba1edf93c4906df9b09058b
bot111111 <107940408+bot111111@users.noreply.github.com>
2022-07-29 13:03:46 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -54,8 +54,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://github.com/arshsisodiya/helios-mirror")
-    buttons.buildbutton("Support Group", "https://t.me/mirrorsociety")
+    buttons.buildbutton("Sledger", "#")
+    buttons.buildbutton("Cloud", "#")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
@@ -188,7 +188,7 @@ help_string = f'''
 Hei, Need Help!!
 '''
 help = telegraph.create_page(
-        title='Helios-Mirror Help',
+        title='Sledger Cloud',
         content=help_string_telegraph + sudo_help_string,
     )["path"]
 
